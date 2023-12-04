f72b916c914f9bafa1dd12ce1fe4993a25e3baf7
ZuhreB <zuhrebezir0@gmail.com>
2023-12-04 23:16:19 +0300

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,11 @@
+public class Player{
+Cards player_cards [] =new Cards[10];
+Cards game_cards [] = new Cards [4];
+private String name;
+int empty_index;
+public Player(String name ){
+this.name =name;
+
+}
+
+}
