5aaf660d6a130c8b4fb1cd95198359e5a4650fba
ZuhreB <zuhrebezir0@gmail.com>
2023-12-11 23:15:31 +0300

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,11 +1,14 @@
 public class Player{
 Cards player_cards [] =new Cards[10];
 Cards game_cards [] = new Cards [4];
+Cards board_cards [] = new Cards[9];
 private String name;
 int empty_index;
+int board_card_index;
+boolean play=true;
 public Player(String name ){
 this.name =name;
-
 }
+
 
 }
