1a6f7321e715f539f95b84b1237b4895c03fcbf2
ZuhreB <zuhrebezir0@gmail.com>
2023-12-11 23:15:10 +0300

    Update Play.java

--- Play.java
+++ Play.java
@@ -2,24 +2,31 @@ public class Play{
 public static void main (String [] args){
 Deck d1 = new Deck();
 System.out.println("Game cards");
-d1.showCard();
+d1.showCard(d1.cards);
 System.out.println();
 System.out.println("shuffled cards");
 d1.shuffle();
-d1.showCard();
+d1.showCard(d1.cards);
 System.out.println();
 System.out.println("delivery");
 d1.delivery(d1.p.player_cards,d1.c.player_cards);
 d1.deliverySpecial();
 System.out.println();
 System.out.println("gamer 1 cards");
-d1.showCardPlayer1();
+d1.showCard(d1.p.player_cards);
 System.out.println();
 System.out.println("gamer 2 cards");
-d1.showCardPlayer2();
+d1.showCard(d1.c.player_cards);
 System.out.println();
 d1.gameCard();
 System.out.println();
 d1.play();
+System.out.println();
+d1.play_computer();
+System.out.println();
+d1.play();
+System.out.println();
+d1.play_computer();
+System.out.println();
 }
 }
