fa6c99761c4625f8c2ecbc533e11b8edafcfe60e
ZuhreB <zuhrebezir0@gmail.com>
2023-12-22 01:52:09 +0300

    Update Play.java

--- Play.java
+++ Play.java
@@ -1,32 +1,54 @@
+import java.util.Scanner;
 public class Play{
 public static void main (String [] args){
+Scanner sc = new Scanner(System.in);
 Deck d1 = new Deck();
+Ruler r= new Ruler();
+System.out.println("please enter your name");
+Player p = new Player(sc.nextLine());
+System.out.println(" your name " +p.getName());
+Player c= new Player("computer");
 System.out.println("Game cards");
-d1.showCard(d1.cards);
+r.showCard(d1.cards);
 System.out.println();
 System.out.println("shuffled cards");
-d1.shuffle();
-d1.showCard(d1.cards);
+r.shuffle(d1.cards);
+r.showCard(d1.cards);
 System.out.println();
 System.out.println("delivery");
-d1.delivery(d1.p.player_cards,d1.c.player_cards);
-d1.deliverySpecial();
+r.delivery(p,c,d1.cards);
+r.deliverySpecial(p,c,d1.cards);
 System.out.println();
 System.out.println("gamer 1 cards");
-d1.showCard(d1.p.player_cards);
+r.showCard(p.player_cards);
 System.out.println();
 System.out.println("gamer 2 cards");
-d1.showCard(d1.c.player_cards);
+r.showCard(c.player_cards);
 System.out.println();
-d1.gameCard();
+r.gameCard(p);
 System.out.println();
-d1.play();
+r.gameCard(c);
 System.out.println();
-d1.play_computer();
-System.out.println();
-d1.play();
-System.out.println();
-d1.play_computer();
-System.out.println();
+int caunter=0;
+for(int i=0;i<d1.cards.length;i++){
+if(d1.cards[i]==null){
+	caunter++;
+}
+}
+while(caunter<=40){
+	r.giveBoard(p,d1.cards);
+	p.play(p);
+	System.out.println();
+	System.out.println("computer playyyyyyyyyyyyyyy");
+	r.giveBoard(c,d1.cards);
+	c.playComputer(c);
+	r.calculate_score(p,c);
+	if(c.score>=20||p.score>=20){
+			break;
+		}
+		
+}
+
+
 }
 }
